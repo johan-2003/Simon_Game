b708776e29cd8e2aed3e046e1128df879cb79187
Johan Suresh <jtsuresh@ucsc.edu>
2022-01-05 17:01:36 -0800

    added event listeners and changed some things

--- game.js
+++ game.js
@@ -1,5 +1,32 @@
-body{
-    text-align: center;
-    background-color: #011F3F;
+const squares = Array.from(document.querySelectorAll('.square'))
+
+
+const buttonColors = ['green', 'red', 'blue', 'yellow']
+const gamePattern = [] //Computer Pattern 
+const userPattern = [] // User Input Pattern to check with game Pattern
+
+
+randomColor = buttonColors[nextSequence()] // So I use nextSequence to select a color from the array
+console.log(randomColor);
+gamePattern.push(randomColor) //push that onto the AI sequence
+
+//Animating the flash
+let buttonToBeFlashed = document.querySelector("." + randomColor)
+console.log(buttonToBeFlashed);
+buttonToBeFlashed.addEventListener("click", () => {
+    buttonToBeFlashed.classList.add("pressed");
+    setTimeout(() => {
+        buttonToBeFlashed.classList.remove("pressed");
+    },100);
+})
+
+
+
+//^^ Works so, idea for AI, to click the button using the click()
+
+function nextSequence() {
+    const RandomNumber = Math.floor(Math.random()*4);
+    return RandomNumber
 }
+
 
