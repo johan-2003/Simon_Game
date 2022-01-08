e79ef09be91e107eba4b73b9ac93fb297933fc40
Johan Suresh <jtsuresh@ucsc.edu>
2022-01-07 18:47:23 -0800

    changed game structure

--- game.js
+++ game.js
@@ -5,6 +5,19 @@ const buttonColors = ['green', 'red', 'blue', 'yellow']
 const gamePattern = [] //Computer Pattern 
 const userPattern = [] // User Input Pattern to check with game Pattern
 
+function nextSequence() {
+    const RandomNumber = Math.floor(Math.random()*4);
+    randomColor = buttonColors[RandomNumber] // So I use nextSequence to select a color from the array
+    console.log(randomColor);
+    gamePattern.push(randomColor); 
+}
+
+const soundMap = {
+    blue: new Audio("sounds/blue.mp3"),
+    green: new Audio("sounds/green.mp3"),
+    red: new Audio("sounds/red.mp3"),
+    yellow: new Audio("sounds/yellow.mp3")
+}
 
 randomColor = buttonColors[nextSequence()] // So I use nextSequence to select a color from the array
 console.log(randomColor);
@@ -15,18 +28,20 @@ let buttonToBeFlashed = document.querySelector("." + randomColor)
 console.log(buttonToBeFlashed);
 buttonToBeFlashed.addEventListener("click", () => {
     buttonToBeFlashed.classList.add("pressed");
+    soundMap[randomColor].play();
     setTimeout(() => {
         buttonToBeFlashed.classList.remove("pressed");
-    },100);
+    },1000);
 })
 
 
 
-//^^ Works so, idea for AI, to click the button using the click()
 
-function nextSequence() {
-    const RandomNumber = Math.floor(Math.random()*4);
-    return RandomNumber
-}
+buttonToBeFlashed.click();
+
+
+//^^ Works so far, idea for AI, to click the button using the click()
+
+
 
 
